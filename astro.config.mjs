f4118cb9a2afcc79af581ebc3dd77a1e8eeeed06
az9702km <az9798mt@gmail.com>
2023-10-25 04:16:05 +0500

    feat: add new pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,24 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  build: {
+    format: "file",
+    assets: "assets",
+  },
+  compressHTML: false,
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: "[name][hash].mjs",
+          chunkFileNames: "chunks/[name].mjs",
+          assetFileNames: "assets/[name][extname]",
+        },
+      },
+    },
+  },
 });
